aa6eeafd436e3bcef88742d0807372f95a94ac45
Zoe McBride <zoe.camille@berkeley.edu>
2016-05-04 13:53:43 -0700

    initial commit 05/04/16

---
+++ WebSite Basics/example.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <!--My CSS and Bootstrap's CSS-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css" />
+    <title>ISAE Alumni Website</title>
+</head>
+<body>
+    <h4>Hello world</h4>
+
+   <!--JS for drop-down menus, at bottom to load faster-->
+   <script src="js/jquery.js"></script>
+   <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
