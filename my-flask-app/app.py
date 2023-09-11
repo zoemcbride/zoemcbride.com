c19711eaeb12d7b0e9123f4e8ea18f755d030e49
Zoe McBride <>
2023-09-11 16:43:53 -0700

    Progress on running blog

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from utils import calculate_reading_time, count_words  
 
 app = Flask(__name__, template_folder='templates')
 
@@ -16,7 +17,20 @@ def side_projects():
 
 @app.route('/running_analysis_blog')
 def running_analysis_blog():
-    return render_template('running_analysis_blog.html')
+    # Specify the path relative to the template folder
+    html_file_path = 'templates/running_analysis_blog.html'  # Adjust the path accordingly
+
+    # Read the content of your running analysis blog post from the HTML file
+    with open(html_file_path, 'r') as blog_file:
+        blog_post_content = blog_file.read() # Note that this technically counts all words on the html file, including html coding
+    
+    # Calculate reading time using the calculate_reading_time function
+    reading_time_minutes = calculate_reading_time(blog_post_content)
+    
+    return render_template('running_analysis_blog.html', content=blog_post_content, word_count=count_words(blog_post_content), reading_time=reading_time_minutes)
+
+
+
 
 if __name__ == '__main__':
     app.run()
